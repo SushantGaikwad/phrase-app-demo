5d5ce8fd849208691b100ae3fb6d57d003f1a53a
SushantGaikwad <sushantgaikwadd06@gmail.com>
2023-08-25 20:17:57 +0530

    French Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useTranslation } from 'react-i18next';
+// import moment from 'moment/moment';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLang = (lang) => {
+    i18n.changeLanguage(lang);
+    // moment.locale('ru');
+  }
   return (
     <div className="App">
-        <h1>Phrase App Demo</h1>
+        <h1>{t('Phrase App Demo')}</h1>
+        <h1>{t('Second EN Key')}</h1>
+        <button onClick={() => changeLang('en')}>Change Lang EN</button>
+        <br />
+        <br />
+        <button onClick={() => changeLang('ru')}>Change Lang RU</button>
+        <br />
+        <br />
+        <button onClick={() => changeLang('fr')}>Change Lang FR</button>
     </div>
   );
 }
